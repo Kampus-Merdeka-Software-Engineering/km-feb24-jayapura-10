6ea780436f2080a71859d2482f3e1a6bb6f28cf3
Ame0thyst <kokoo15011209@gmail.com>
2024-05-12 06:18:43 +0700

    feat(navbar) : menambahkan navbar

--- FE/js/index.js
+++ FE/js/index.js
@@ -1,18 +1,54 @@
+// JavaScript
 document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll('nav ul li a');
-
-    links.forEach(link => {
-        link.addEventListener('click', smoothScroll);
-    });
-
-    function smoothScroll(e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
+    const navLinks = document.querySelectorAll("#menu li a");
+  
+    navLinks.forEach(function(navLink) {
+      navLink.addEventListener("click", function(event) {
+        event.preventDefault();
+  
+        const targetId = this.getAttribute("href").substring(1);
         const targetElement = document.getElementById(targetId);
         const offsetTop = targetElement.offsetTop;
+  
         window.scrollTo({
-            top: offsetTop,
-            behavior: 'smooth'
+          top: offsetTop,
+          behavior: "smooth"
         });
-    }
-});
+      });
+    });
+  });
+  
+
+
+// Toggle Menu Icon ========================================
+// let toggleIcon = document.querySelector('.menuIcon');
+
+// toggleIcon.addEventListener('click', () => {
+//     if (toggleIcon.className != 'menuIcon toggle') {
+//         toggleIcon.className += ' toggle';
+//     } else {
+//         toggleIcon.className = 'menuIcon';
+//     }
+// });
+
+// /*  batas */
+// document.addEventListener("DOMContentLoaded", function() {
+//     const navLinks = document.querySelectorAll('.link');
+  
+//     navLinks.forEach(link => {
+//       link.addEventListener('click', scrollToSection);
+//     });
+  
+//     function scrollToSection(event) {
+//       event.preventDefault();
+//       const targetId = this.getAttribute('href').substring(1);
+//       const targetSection = document.getElementById(targetId);
+//       const distanceToTop = targetSection.offsetTop;
+  
+//       window.scrollTo({
+//         top: distanceToTop,
+//         behavior: 'smooth'
+//       });
+//     }
+//   });
+  
