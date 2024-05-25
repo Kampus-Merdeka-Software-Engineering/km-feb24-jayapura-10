01600cd42f6a02ffc489544766501541bdda0bc9
Rinaldi Mulya  Pratama <105484093+RinaldiMulya@users.noreply.github.com>
2024-05-25 22:27:10 +0700

    pembaharuan section team

--- FE/js/team.js
+++ FE/js/team.js
@@ -1,15 +1,22 @@
-let next = document.querySelector('.next')
-let prev = document.querySelector('.prev')
+// Mengatur latar belakang untuk setiap elemen .team-item
+document.querySelectorAll('.team-item').forEach(item => {
+  const background = item.getAttribute('data-background');
+  item.style.backgroundImage = `url(${background})`;
+});
 
-next.addEventListener('click', function(){
-  let items = document.querySelectorAll('.team-item')
-  document.querySelector('.team-slide').appendChild(items[0])
-})
+// Mengatur event listener untuk tombol next dan prev
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
 
-prev.addEventListener('click', function(){
-  let items = document.querySelectorAll('.team-item')
-  document.querySelector('.team-slide').prepend(items[items.length - 1])
-})
+next.addEventListener('click', function() {
+  let items = document.querySelectorAll('.team-item');
+  document.querySelector('.team-slide').appendChild(items[0]);
+});
+
+prev.addEventListener('click', function() {
+  let items = document.querySelectorAll('.team-item');
+  document.querySelector('.team-slide').prepend(items[items.length - 1]);
+});
 
 document.addEventListener('keydown', function(event) {
   if (event.key === "ArrowRight") {
