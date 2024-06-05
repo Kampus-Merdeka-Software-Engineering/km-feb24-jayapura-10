43c7b8fca10644d3f2f80e5dfc2237d7eb55c72e
Ame0thyst <kokoo15011209@gmail.com>
2024-06-06 03:40:31 +0700

    🐛fix(index.js): tidak bisa berpindah walau sudah diberi link

--- FE/js/index.js
+++ FE/js/index.js
@@ -1,42 +1,26 @@
-// JavaScript untuk scrolling menu mulus by id
-  document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('nav a').forEach(anchor => {
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('nav a').forEach(anchor => {
       anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-  
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-  
-        targetElement.scrollIntoView({ behavior: 'smooth' });
+          const href = this.getAttribute('href');
+          if (href.startsWith('#')) {
+              e.preventDefault();
+              const targetId = href.substring(1);
+              const targetElement = document.getElementById(targetId);
+              if (targetElement) {
+                  targetElement.scrollIntoView({ behavior: 'smooth' });
+              }
+          }
+          // Jika href tidak dimulai dengan '#', biarkan default action terjadi
       });
-    });
   });
-  
-  
-
-  /* rezie ukuran dashboard*/ 
-  document.addEventListener('DOMContentLoaded', function() {
-    var iframe = document.querySelector('iframe');
-    function resizeIframe() {
-      var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-      iframe.style.height = height + 'px';
-    }
-  
-    window.addEventListener('resize', resizeIframe);
-    resizeIframe(); // Adjust height on load
-  });
-
-  /*  hampir sama dengan yang diatas, hanya saja ini utnuk yang footernya*/
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-  
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
-  });
+});
 
 
- 
-  
+document.addEventListener('DOMContentLoaded', () => {
+const button = document.getElementById('button-home');
+button.addEventListener('click', () => {
+    window.location.href = 'ht.html';  // Ganti dengan tautan yang diinginkan
+});
+});
+
